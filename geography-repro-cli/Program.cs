6a26df643ae8c87acbe9357459c53309765aeae5
Mark Jordan <mark.jordan@red-gate.com>
2018-03-07 15:15:59 +0000

    initial commit: trying to create minimal example for problem

---
+++ geography-repro-cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using geography_repro;
+
+namespace geography_repro_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: geography-repro-cli [connection string]");
+            }
+
+            var connectionString = args[0];
+
+            try
+            {
+                var result = GeographySelector.GetGeographyResult(connectionString);
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ geography-repro/GeographySelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace geography_repro
+{
+    public static class GeographySelector
+    {
+        public static string GetGeographyResult(string connectionString)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("SELECT geo from geo_table", connection))
+                {
+                    return command.ExecuteScalar().ToString();
+                }
+            }
+        }
+    }
+}
